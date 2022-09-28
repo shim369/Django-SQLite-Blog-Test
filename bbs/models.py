0e1c7df1ca0341ed001f2e049e8ac43945b07d20
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-09-28 17:12:26 +0900

    DBにカラム追加

--- bbs/models.py
+++ bbs/models.py
@@ -1,8 +1,14 @@
+from turtle import title
 from django.db import models
 
 # 投稿内容を格納するモデルを定義
 class Article(models.Model):
-  content = models.CharField(max_length=200)
+  title = models.CharField('タイトル', max_length=200, null=True)
+  content = models.TextField('テキスト')
+  created_at = models.DateField('作成日', auto_now_add=True)
+  updated_at = models.DateField('更新日', auto_now=True)
+  is_publick = models.BooleanField('公開する', default=False, help_text='公開する場合はチェックを入れてください')
+
 
   def __str__(self):
     return self.content
