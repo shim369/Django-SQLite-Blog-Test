852a8f8832a8b4c14d46ae60cc92b5d082a089c7
shimpei ohtani <ohtani.shimpei@gmail.com>
2022-11-10 13:26:29 +0900

    pagetop delete

--- static/js/main.js
+++ static/js/main.js
@@ -32,18 +32,18 @@ $(function () {
    }
 });
 
-$(function(){
-   let pageTop = $('#page-top');
-   pageTop.hide();
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-         pageTop.fadeIn();
-      } else {
-         pageTop.fadeOut();
-      }
-   });
-   pageTop.click(function () {
-      $('body, html').animate({ scrollTop: 0 }, 500);
-      return false;
-   });
-});
+// $(function(){
+//    let pageTop = $('#page-top');
+//    pageTop.hide();
+//    $(window).scroll(function () {
+//       if ($(this).scrollTop() > 100) {
+//          pageTop.fadeIn();
+//       } else {
+//          pageTop.fadeOut();
+//       }
+//    });
+//    pageTop.click(function () {
+//       $('body, html').animate({ scrollTop: 0 }, 500);
+//       return false;
+//    });
+// });
